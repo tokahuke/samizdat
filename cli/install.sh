886990e4ee95d136a380f00744814bf118dd920d
Pedro Arruda <pedroarruda@Pedros-MacBook-Pro.local>
2022-04-04 00:03:36 +0200

    created launchd daemon

--- cli/install.sh
+++ cli/install.sh
@@ -2,4 +2,8 @@
 
 # A quick and dirty script to build and install the samizdat CLI
 
-cargo build && cp ../target/debug/samizdat $HOME/.local/bin
+if [ "$(uname)" == "Darwin" ]; then
+    cargo build && sudo cp ../target/debug/samizdat /usr/local/bin
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    cargo build && cp ../target/debug/samizdat $HOME/.local/bin
+fi
--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/env bash
 
-sudo systemctl disable --now samizdat-node
-sudo rm /usr/local/bin/samizdat-node
-sudo rm -rf /var/samizdat/node
-sudo rmdir /var/samizdat || "samizdat folder not empty"
+if [ "$(uname)" == "Darwin" ]; then
+    
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then    
+    sudo systemctl disable --now samizdat-node
+    sudo rm /usr/local/bin/samizdat-node
+    sudo rm -rf /var/samizdat/node
+    sudo rmdir /var/samizdat || "samizdat folder not empty"
+fi
