9e92c6f9d225a20a5b9ac617bed322079e847cb7
Pedro Arruda <parruda@findhotel.net>
2022-09-01 15:09:18 -0300

    Revert "linuz install script debugged"
    This reverts commit 79ee948b9fce018c9c4a4789c5e11aec9fbed14f.

--- install/node/x86_64-unknown-linux-gnu/install.sh
+++ install/node/x86_64-unknown-linux-gnu/install.sh
@@ -5,7 +5,7 @@ if [ "$(expr substr $(uname -s) 1 5)" != "Linux" ]; then
     exit 1
 fi
 
-urlprefix=https://proxy.hubfederation.com/_series/$SAMIZDAT_PUBLIC_KEY/$VERSION/node/x86_64-unknown-linux-gnu
+urlprefix=http://proxy.hubfederation.com/_series/$SAMIZDAT_PUBLIC_KEY/$VERSION/node/x86_64-unknown-linux-gnu
 tmpdir=/tmp/samizdat-install-$RANDOM
 
 mkdir -p $tmpdir &&
@@ -15,11 +15,9 @@ curl $urlprefix/samizdat-node > samizdat-node &&
 curl $urlprefix/samizdat > samizdat &&
 curl $urlprefix/samizdat-node.service > samizdat-node.service &&
 
-(systemctl stop samizdat-node || echo 'No running node detected') &&
 cp samizdat-node /usr/local/bin &&
 cp samizdat /usr/local/bin &&
 cp samizdat-node.service /etc/systemd/system/samizdat-node.service &&
-systemctl daemon-reload &&
 systemctl enable --now samizdat-node &&
 
 rm -rf $tmpdir
