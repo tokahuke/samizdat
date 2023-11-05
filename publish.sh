1c3812abd498fa9b56210b6887abde913b3cdc51
Pedro Arruda <parruda@findhotel.net>
2023-11-04 22:08:26 -0300

    Improve publish.sh

--- publish.sh
+++ publish.sh
@@ -6,10 +6,12 @@
 #
 ###
 
+set -e
+
 export CURRENT_BRANCH=`git branch --show-current`
 
-git push &&
-git checkout stable &&
-git merge main &&
-git push &&
+git push
+git checkout stable
+git merge main
+git push
 git checkout $CURRENT_BRANCH
