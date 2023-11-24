f1bba721072883d2ce8747d9a7d5583e22e31d96
Pedro Arruda <parruda@findhotel.net>
2023-11-24 17:50:54 -0300

    Small improvement to windows service

--- install/node/x86_64-pc-windows-gnu/samizdat-service/src/main.rs
+++ install/node/x86_64-pc-windows-gnu/samizdat-service/src/main.rs
@@ -1,7 +1,9 @@
 #[cfg(target_os = "windows")]
 fn main() -> Result<(), windows_service::Error> {
     use std::ffi::OsString;
-    use std::process::Command;
+    use std::fs::File;
+    use std::fs::File;
+    use std::process::{Command, Stdio};
 
     use windows_service::define_windows_service;
     use windows_service::service_dispatcher;
@@ -9,35 +11,53 @@ fn main() -> Result<(), windows_service::Error> {
     define_windows_service!(ffi_service_main, my_service_main);
 
     pub fn my_service_main(arguments: Vec<OsString>) {
-        // The entry point where execution will start on a background thread after a call to
-        // `service_dispatcher::start` from `main`.
-        let mut command = Command::new("samizdat-node.exe");
-        command.args(arguments.clone());
-
-        match command.spawn() {
-            Ok(mut child) => match child.wait() {
-                Ok(status) if status.success() => {
-                    eprintln!(
-                        "samizdat-node.exe {:?} exited with status {status}",
-                        arguments
-                    )
-                }
-                Ok(status) => {
-                    eprintln!(
-                        "samizdat-node.exe {:?} failed with status {status}",
-                        arguments
-                    )
-                }
-                Err(err) => {
-                    eprintln!(
-                        "Failed to wait for samizdat-node.exe {:?}: {err}",
-                        arguments
-                    );
-                }
-            },
-            Err(err) => {
-                eprintln!("Failed to invoke samizdat-node.exe {:?}: {err}", arguments);
+        let maybe_create_file = |name: &str| {
+            if let Ok(file) = File::create(name) {
+                Stdio::from(file)
+            } else {
+                eprintln!("Could not create {name} stdio! Will inherit from the current process");
+                Stdio::inherit()
             }
+        };
+        let stdout_log = maybe_create_file(r"C:\ProgramData\Samizdat\Node\stdout.log");
+        let stderr_log = maybe_create_file(r"C:\ProgramData\Samizdat\Node\stderr.log"); 
+
+        loop {
+            // The entry point where execution will start on a background thread after a call to
+            // `service_dispatcher::start` from `main`.
+            let mut command = Command::new("samizdat-node.exe");
+            command.args(arguments.clone());
+            command.env("RUST_BACKTRACE", "1");
+            command.stdout(stdout);
+            command.stderr(stderr);
+
+            match command.spawn() {
+                Ok(mut child) => match child.wait() {
+                    Ok(status) if status.success() => {
+                        eprintln!(
+                            "samizdat-node.exe {:?} exited with status {status}",
+                            arguments
+                        )
+                    }
+                    Ok(status) => {
+                        eprintln!(
+                            "samizdat-node.exe {:?} failed with status {status}",
+                            arguments
+                        )
+                    }
+                    Err(err) => {
+                        eprintln!(
+                            "Failed to wait for samizdat-node.exe {:?}: {err}",
+                            arguments
+                        );
+                    }
+                },
+                Err(err) => {
+                    eprintln!("Failed to invoke samizdat-node.exe {:?}: {err}", arguments);
+                }
+            }
+
+            std::thread::sleep(std::time::Duration::from_secs(1));
         }
     }
 
@@ -48,6 +68,6 @@ fn main() -> Result<(), windows_service::Error> {
 }
 
 #[cfg(not(target_os = "windows"))]
-fn main() -> Result<(), ()> {
-    panic!("This program only makes sense in Windows");
+fn main() -> ! {
+    panic!("This program only makes sense in Windows")
 }
