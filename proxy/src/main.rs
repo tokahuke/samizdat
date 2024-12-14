f9875ff73117056c8b84554c71c57d141a7b6ae7
Pedro Arruda <parruda@findhotel.net>
2024-12-14 09:11:12 -0300

    simplify proxy

--- proxy/src/main.rs
+++ proxy/src/main.rs
@@ -2,9 +2,7 @@ mod cli;
 mod html;
 mod http;
 
-use std::time::Duration;
-
-use axum_server::{tls_rustls::RustlsConfig, Handle};
+use axum_server::tls_rustls::RustlsConfig;
 use cli::cli;
 
 #[tokio::main]
@@ -18,36 +16,31 @@ async fn main() -> Result<(), anyhow::Error> {
             .install_default()
             .expect("failed to install crypto provider `aws_lc_rs`");
 
-        loop {
-            // Run certbot:
-            let status = std::process::Command::new("certbot")
-                .arg("certonly")
-                .arg("--standalone")
-                .arg("--non-interactive")
-                .arg("--email")
-                .arg(cli().owner()?)
-                .arg("--agree-tos")
-                .arg("--domain")
-                .arg(cli().domain()?)
-                .spawn()
-                .expect("failed to spawn certbot")
-                .wait()
-                .expect("failed to run certbot");
-            assert_eq!(status.code().expect("there always is a code (?)"), 0);
+        // Run certbot:
+        let status = std::process::Command::new("certbot")
+            .arg("certonly")
+            .arg("--standalone")
+            .arg("--non-interactive")
+            .arg("--email")
+            .arg(cli().owner()?)
+            .arg("--agree-tos")
+            .arg("--domain")
+            .arg(cli().domain()?)
+            .spawn()
+            .expect("failed to spawn certbot")
+            .wait()
+            .expect("failed to run certbot");
+        assert_eq!(status.code().expect("there always is a code (?)"), 0);
 
-            let config = RustlsConfig::from_pem_file(
-                format!("/etc/letsencrypt/live/{}/fullchain.pem", cli().domain()?),
-                format!("/etc/letsencrypt/live/{}/privkey.pem", cli().domain()?),
-            )
+        let config = RustlsConfig::from_pem_file(
+            format!("/etc/letsencrypt/live/{}/fullchain.pem", cli().domain()?),
+            format!("/etc/letsencrypt/live/{}/privkey.pem", cli().domain()?),
+        )
+        .await?;
+
+        axum_server::bind_rustls(([0, 0, 0, 0], cli().port.unwrap_or(443)).into(), config)
+            .serve(crate::http::api().into_make_service())
             .await?;
-            let handle = Handle::new();
-            handle.graceful_shutdown(Some(Duration::from_secs(60 * 24 * 60 * 60)));
-
-            axum_server::bind_rustls(([0, 0, 0, 0], cli().port.unwrap_or(443)).into(), config)
-                .handle(handle)
-                .serve(crate::http::api().into_make_service())
-                .await?;
-        }
     } else {
         // Start server:
         axum::serve(
